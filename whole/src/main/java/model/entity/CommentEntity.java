0b5facba3001ca65c9a8cd0ab44e83f592756c82
Parkseojin <seojin5565@gmail.com>
2023-11-27 20:27:58 +0900

    [feat] : commentEntity 필드추 #19

--- whole/src/main/java/model/entity/CommentEntity.java
+++ whole/src/main/java/model/entity/CommentEntity.java
@@ -1,5 +1,51 @@
 package model.entity;
 
+import java.time.LocalDate;
+
 public class CommentEntity {
+//    POST_ID
+//    REG_DATE
+//    COMMENT_ID
+//    USER_ID
+//    COMMENT_CONTENT
+//    
+    private Long postId;
+    private LocalDate regDate;
+    private Long commentId;
+    private Long userID;
+    private String content;
+    public Long getPostId() {
+        return postId;
+    }
+    public void setPostId(Long postId) {
+        this.postId = postId;
+    }
+    public LocalDate getRegDate() {
+        return regDate;
+    }
+    public void setRegDate(LocalDate regDate) {
+        this.regDate = regDate;
+    }
+    public Long getCommentId() {
+        return commentId;
+    }
+    public void setCommentId(Long commentId) {
+        this.commentId = commentId;
+    }
+    public Long getUserID() {
+        return userID;
+    }
+    public void setUserID(Long userID) {
+        this.userID = userID;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+    
+    
+
 
 }
