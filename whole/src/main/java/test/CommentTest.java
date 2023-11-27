8e623907d3c59e18196e05994fd53c531c800d54
Parkseojin <seojin5565@gmail.com>
2023-11-27 21:15:44 +0900

    [add] : commentTest 중간과정 #19

--- whole/src/main/java/test/CommentTest.java
+++ whole/src/main/java/test/CommentTest.java
@@ -1,10 +1,35 @@
 package test;
 
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Scanner;
+
+import model.dao.CommentDAO;
+import model.entity.CommentEntity;
+
 public class CommentTest {
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+    static CommentDAO commentDAO = new CommentDAO();
+    static Scanner sc = new Scanner(System.in);
+    
+    public static void main(String[] args) throws SQLException {
+        
+        //출력 
+        printCommentList();
+        
+        // 입력
+        
+        // 삭
+        
+        
+    }
+    
+    public static void printCommentList() throws SQLException {
+        System.out.print("조회할 postId 입력 : ");
+        List<CommentEntity> commentList = commentDAO.printCommentList(sc.nextLong());
+        
+        
+        
     }
 
 }
