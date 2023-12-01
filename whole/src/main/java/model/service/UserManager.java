f7ecd1fbd7e1c78da630232a3dd4f5a0c34954a4
Dayun Lee <wohd7877@naver.com>
2023-12-02 01:35:54 +0900

    [add] : loginController추가

--- whole/src/main/java/model/service/UserManager.java
+++ whole/src/main/java/model/service/UserManager.java
@@ -1,12 +1,12 @@
-//package model.service;
+package model.service;
 //
-//import java.sql.SQLException;
-//import java.util.List;
+import java.sql.SQLException;
+import java.util.List;
 //
-//import model.Community;
-//import model.User;
-//import model.dao.CommunityDAO;
-//import model.dao.UserDAO;
+
+
+import model.dao.UserDAO;
+import model.entity.UserEntity;
 //
 ///**
 // * 사용자 관리 API를 사용하는 개발자들이 직접 접근하게 되는 클래스.
@@ -15,47 +15,48 @@
 // * 비지니스 로직이 복잡한 경우에는 비지니스 로직만을 전담하는 클래스를 
 // * 별도로 둘 수 있다.
 // */
-//public class UserManager {
-//	private static UserManager userMan = new UserManager();
-//	private UserDAO userDAO;
+public class UserManager {
+	private static UserManager userMan = new UserManager();
+	private UserDAO userDAO;
 //	private CommunityDAO commDAO;
 //	private UserAnalysis userAanlysis;
 //
-//	private UserManager() {
-//		try {
-//			userDAO = new UserDAO();
+	private UserManager() {
+		try {
+			userDAO = new UserDAO();
 //			commDAO = new CommunityDAO();
 //			userAanlysis = new UserAnalysis(userDAO);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}			
-//	}
-//	
-//	public static UserManager getInstance() {
-//		return userMan;
-//	}
-//	
-//	public int create(User user) throws SQLException, ExistingUserException {
-//		if (userDAO.existingUser(user.getUserId()) == true) {
-//			throw new ExistingUserException(user.getUserId() + "는 존재하는 아이디입니다.");
-//		}
-//		return userDAO.create(user);
-//	}
-//
-//	public int update(User user) throws SQLException, UserNotFoundException {
+		} catch (Exception e) {
+			e.printStackTrace();
+		}			
+	}
+	
+	public static UserManager getInstance() {
+		return userMan;
+	}
+	
+	public int create(UserEntity user) throws SQLException, ExistingUserException {
+		if (userDAO.existingUser(user.getEmail()) == true) {
+			throw new ExistingUserException(user.getEmail() + "는 존재하는 아이디입니다.");
+		}
+		return userDAO.create(user);
+	}
+
+	public int update(UserEntity user) throws SQLException, UserNotFoundException, ExistingUserException {
 //		int oldCommId = findUser(user.getUserId()).getCommId();
-//		if (user.getCommId() != oldCommId) { 	// 소속 커뮤티니가 변경됨
+		if (userDAO.existingUser(user.getEmail()) == false) { 	
 //			Community comm = commDAO.findCommunity(oldCommId);  // 기존 소속 커뮤니티
 //			if (comm != null && user.getUserId().equals(comm.getChairId())) {
 //				// 사용자가 기존 소속 커뮤니티의 회장인 경우 -> 그 커뮤니티의 회장을 null로 변경 및 저장
 //				comm.setChairId(null);
 //				commDAO.updateChair(comm);
 //			}
-//		}
-//		return userDAO.update(user);
-//	}	
+		    throw new ExistingUserException(user.getEmail() + "는 존재하는 아이디입니다.");
+		}
+		return userDAO.update(user);
+	}	
 //
-//	public int remove(String userId) throws SQLException, UserNotFoundException {
+	public int remove(String email) throws SQLException, UserNotFoundException {
 //		int commId = findUser(userId).getCommId();
 //		Community comm = commDAO.findCommunity(commId);  // 소속 커뮤니티
 //		if (comm != null && userId.equals(comm.getChairId())) {
@@ -63,37 +64,37 @@
 //			comm.setChairId(null);
 //			commDAO.updateChair(comm);
 //		}
-//		return userDAO.remove(userId);
-//	}
+		return userDAO.remove(email);
+	}
 //
-//	public User findUser(String userId)
-//		throws SQLException, UserNotFoundException {
-//		User user = userDAO.findUser(userId);
-//		
-//		if (user == null) {
-//			throw new UserNotFoundException(userId + "는 존재하지 않는 아이디입니다.");
-//		}		
-//		return user;
-//	}
+	public UserEntity findUser(String email)
+		throws SQLException, UserNotFoundException {
+		UserEntity user = userDAO.findUser(email);
+		
+		if (user == null) {
+			throw new UserNotFoundException(email + "는 존재하지 않는 아이디입니다.");
+		}		
+		return user;
+	}
 //
-//	public List<User> findUserList() throws SQLException {
-//			return userDAO.findUserList();
-//	}
+	public List<UserEntity> findUserList(String email) throws SQLException {
+			return userDAO.findUserList(email);
+	}
 //	
 //	public List<User> findUserList(int currentPage, int countPerPage)
 //		throws SQLException {
 //		return userDAO.findUserList(currentPage, countPerPage);
 //	}
 //
-//	public boolean login(String userId, String password)
-//		throws SQLException, UserNotFoundException, PasswordMismatchException {
-//		User user = findUser(userId);
+	public boolean login(String email, String password)
+		throws SQLException, UserNotFoundException, PasswordMismatchException {
+		UserEntity user = findUser(email);
 //
-//		if (!user.matchPassword(password)) {
-//			throw new PasswordMismatchException("비밀번호가 일치하지 않습니다.");
-//		}
-//		return true;
-//	}
+		if (!user.matchPassword(password)) {
+			throw new PasswordMismatchException("비밀번호가 일치하지 않습니다.");
+		}
+		return true;
+	}
 //
 //	//public List<User> makeFriends(String userId) throws Exception {
 //	//	return userAanlysis.recommendFriends(userId);
@@ -122,11 +123,12 @@
 //		return commDAO.findCommunityList();
 //	}
 //	
-//	//p/ublic List<User> findCommunityMembers(int commId) throws SQLException {
+//	//public List<User> findCommunityMembers(int commId) throws SQLException {
 //	//	return userDAO.findUsersInCommunity(commId);
 //	//}
 //
 //	public UserDAO getUserDAO() {
 //		return this.userDAO;
 //	}
-//}
+
+}
