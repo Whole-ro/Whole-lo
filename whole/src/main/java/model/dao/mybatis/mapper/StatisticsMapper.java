cc1b24ac0f0ee0079ea5982ad4e72fb64af620fc
dev_me <gjalsdud1030@naver.com>
2023-12-17 18:08:25 +0900

    [add] : StatisticsMapper interface 구현 #25

--- whole/src/main/java/model/dao/mybatis/mapper/StatisticsMapper.java
+++ whole/src/main/java/model/dao/mybatis/mapper/StatisticsMapper.java
@@ -1,5 +1,17 @@
 package model.dao.mybatis.mapper;
 
-public interface StatisticsMapper {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Select;
+
+import model.dto.FoodDTO;
+
+public interface StatisticsMapper {
+	@Select({ "SELECT title\r\n"
+			+ "FROM FOOD\r\n"
+			+ "JOIN POST USING (post_id)\r\n"
+			+ "WHERE is_healthy = 'BLUE'\r\n"
+			+ "ORDER BY DBMS_RANDOM.VALUE\r\n"
+			+ "FETCH FIRST 5 ROWS ONLY" })
+	List<FoodDTO> selectBluetypeFood();
 }
