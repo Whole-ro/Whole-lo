fd47d0241ed1a74b16d155aa15b2e2b1b2ec4fe2
yjin-jo <dpwls5100@icloud.com>
2023-12-17 17:24:15 +0900

    [feat] : test파일 수정 #25

--- whole/src/main/java/test/FoodTest.java
+++ whole/src/main/java/test/FoodTest.java
@@ -21,33 +21,6 @@ public class FoodTest {
 		Scanner scanner = new Scanner(System.in);
 
 
-
-		//        System.out.print("부서명을 입력하시오: ");
-		//
-		//        String deptName = scanner.next();
-		//
-		//        System.out.println();
-		//
-		//        
-		//
-		//         dept = compDao.findDeptByName(deptName);         
-		//
-		//        // dept 객체의 필드 값 출력 ...
-		//
-		//        System.out.println("<부서정보>");
-		//
-		//        System.out.println("부서번호: " + dept.getDeptNo());
-		//
-		//        System.out.println("부서명: " + deptName);
-		//
-		//        System.out.println("관리자사번: " + dept.getMgrNo());
-		//
-		//        System.out.println("사원 수: " + dept.getNumOfEmps());
-		//
-		//        System.out.println();
-
-
-
 		List<FoodDTO> foodList = foodDao.findFoodList();
 		// empList에 포함된 모든 emp 객체들의 필드 값을 출력 
 
@@ -65,7 +38,7 @@ public class FoodTest {
 
 			FoodDTO food = iter.next();
 
-			System.out.println(food.getTitle());
+			System.out.println(food.getTitle()+food.getFoodType()+food.getContent()+food.getImage()+food.getPostId());
 
 		}
 
@@ -86,7 +59,7 @@ public class FoodTest {
 
 			FoodDTO food = iter.next();
 
-			System.out.println(food.getTitle());
+			System.out.println(food.getTitle()+food.getFoodType()+food.getContent()+food.getImage()+food.getPostId()+food.getWriterId());
 
 		}
 
@@ -104,38 +77,9 @@ public class FoodTest {
 
 			FoodDTO food = iter.next();
 
-			System.out.println(food.getTitle());
+			System.out.println(food.getTitle()+food.getFoodType()+food.getContent()+food.getImage()+food.getPostId());
 
-		}
-		//        System.out.print("새 관리자 사번과 관리자 보직수당을 입력하시오: ");
-		//
-		//        int managerNo  = scanner.nextInt();
-		//
-		//        double commission = scanner.nextDouble();
-		//
-		//        
-		//
-		//        Appointment appo = new Appointment(dept.getDeptNo(), managerNo, commission);
-		//
-		//        Employee oldMgr = compDao.replaceManagerOfDept(appo);    
-		//
-		//     
-		//
-		//        System.out.println("기존 관리자:");     
-		//
-		//        // oldMgr의 필드 값 출력
-		//
-		//        System.out.printf("%d %s %s %s %.2f %.2f %d\n",
-		//
-		//                oldMgr.getEmpno(), oldMgr.getEname(), oldMgr.getJob(),
-		//
-		//                oldMgr.getHiredate(), oldMgr.getSal(), oldMgr.getComm(), oldMgr.getDeptNo());
-		//
-		//        
-		//
-		//        Employee newMgr = compDao.findEmployee(managerNo);   
-		//
-		//        System.out.println("새 관리자:");         
+		}    
 		//
 		//        // newMgr의 필드 값 출력 
 		//
