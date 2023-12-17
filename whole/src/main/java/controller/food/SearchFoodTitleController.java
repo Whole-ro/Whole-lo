d5872f0b3370ec3acb1b69b6a66016f8178d1091
Dayun Lee <wohd7877@naver.com>
2023-12-17 15:32:13 +0900

    [add]:SearchFoodTitleController 추가 #29

---
+++ whole/src/main/java/controller/food/SearchFoodTitleController.java
@@ -0,0 +1,24 @@
+package controller.food;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import controller.Controller;
+import model.dto.FoodDTO;
+import model.service.FoodManager;
+
+public class SearchFoodTitleController implements Controller {
+    
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String title = request.getParameter("title");
+        FoodManager foodMan = FoodManager.getInstance();
+        List<FoodDTO> foodList = foodMan.searchByTitle(title);
+        
+        // commList 객체를 request에 저장하여 커뮤니티 리스트 화면으로 이동(forwarding)
+        request.setAttribute("foodList", foodList);             
+        return "/myRefg/myRefgList.jsp";        
+    }
+}
