0ba1d9f182a33d60fa7db908ecf6d4a76f0f498e
yjin-jo <dpwls5100@icloud.com>
2023-12-17 16:27:04 +0900

    [add] : red 음식 출력 mapper 추가 #25

--- whole/src/main/java/model/dao/mybatis/mapper/StatisticsMapper.java
+++ whole/src/main/java/model/dao/mybatis/mapper/StatisticsMapper.java
@@ -1,5 +1,11 @@
 package model.dao.mybatis.mapper;
+import java.util.List;
 
+import org.apache.ibatis.annotations.Select;
+
+import model.dto.FoodDTO;
 public interface StatisticsMapper {
-
+	
+	@Select({"SELECT title FROM FOOD JOIN POST USING (post_id) WHERE is_healthy = 'RED'"})
+	List<FoodDTO> selectRedByUserId(Long userId);
 }
