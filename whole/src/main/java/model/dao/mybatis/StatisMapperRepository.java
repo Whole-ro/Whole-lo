388a33b008ea11d6b96fef2418874699b1b87551
yjin-jo <dpwls5100@icloud.com>
2023-12-17 16:27:42 +0900

    [add] : red 음식 출력 repo 구현 #25

---
+++ whole/src/main/java/model/dao/mybatis/StatisMapperRepository.java
@@ -0,0 +1,39 @@
+package model.dao.mybatis;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import model.dao.mybatis.mapper.StatisticsMapper;
+import model.dto.FoodDTO;
+
+public class StatisMapperRepository {
+
+	private SqlSessionFactory sqlSessionFactory;
+	
+	public StatisMapperRepository() {
+		String resource = "mybatis-config.xml";
+		InputStream inputStream;
+		try {
+			inputStream = Resources.getResourceAsStream(resource);
+		} catch (IOException e) {
+			throw new IllegalArgumentException(e);
+		}
+		sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+	}
+	
+	public List<FoodDTO> selectRedByUserId(Long userId) {
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		try {
+			return sqlSession.getMapper(StatisticsMapper.class).selectRedByUserId(userId);
+		} finally {
+			sqlSession.close();
+		}
+		
+	}
+}
