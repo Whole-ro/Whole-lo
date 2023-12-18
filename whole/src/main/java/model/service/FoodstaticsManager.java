0f152174581bd8dd3fbc20e39b7395461732be92
dev_me <gjalsdud1030@naver.com>
2023-12-18 15:42:31 +0900

    [add] : foodstaticsManager 파일 추가 #25

--- whole/src/main/java/model/service/FoodstaticsManager.java
+++ whole/src/main/java/model/service/FoodstaticsManager.java
@@ -1,13 +1,31 @@
 package model.service;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import model.dao.mybatis.StatisMapperRepository;
+import model.dto.FoodDTO;
 
 public class FoodstaticsManager {
 
 	private static FoodstaticsManager fstaticsMan = new FoodstaticsManager();
 	private StatisMapperRepository statismapperDAO;
-	
-	
-	
-	
+
+	private FoodstaticsManager() {
+		try {
+			statismapperDAO = new StatisMapperRepository();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
+
+	public static FoodstaticsManager getInstance() {
+		return fstaticsMan;
+	}
+
+	public List<FoodDTO> viewstaticlist() throws SQLException {
+		return statismapperDAO.findMyBLUETypeFood();
+	}
+
+}
