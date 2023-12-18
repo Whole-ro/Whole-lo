251eab2d21f269df3e594dc8e318fcda8caa7703
Parkseojin <seojin5565@gmail.com>
2023-12-18 14:57:42 +0900

    [feat] : FoodCreateController #37

--- whole/src/main/java/controller/food/FoodCreateController.java
+++ whole/src/main/java/controller/food/FoodCreateController.java
@@ -4,44 +4,44 @@ import java.time.LocalDate;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import controller.Controller;
+import controller.user.UserSessionUtils;
 import model.dto.FoodDTO;
 import model.enums.IsHealthy;
 import model.service.FoodManager;
+import util.FoodTypeUtil;
 
 public class FoodCreateController implements Controller {
     
-  private static final Logger log = LoggerFactory.getLogger(FoodCreateController.class);
+//  private static final Logger log = LoggerFactory.getLogger(FoodCreateController.class);
 
   @Override
   public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
    
-//      this.title = title;
-//      this.content = content;
-//      this.image = image;
-//      this.writerId = writerId;
-//      this.foodType = foodType;
-//      this.expDate = expDate;
-//      this.isHealthy = isHealthy; 
-//  }
+      HttpSession session = request.getSession();
+     
+      IsHealthy isHealtyh = FoodTypeUtil.getFoodColor(request.getParameter("foodType"));
       FoodDTO newFood = new FoodDTO(
               request.getParameter("title"), 
               request.getParameter("content"),
               request.getParameter("image"),
-              Long.parseLong(request.getParameter("writerId")), 
-              request.getParameter("foodType"), 
+              Long.parseLong(UserSessionUtils.getLoginUserId(session)),
+              request.getParameter("foodType"),
               LocalDate.parse(request.getParameter("expDate")), 
-              Enum.valueOf(IsHealthy.class, request.getParameter("isHealthy"))
+              isHealtyh
       );
       
+      System.out.println(newFood.toString());
     try {
         
         FoodManager foodMan = FoodManager.getInstance();
         foodMan.createFood(newFood);
         
-        log.debug("Post new Food : {}", newFood);
+//        log.debug("Post new Food : {}", newFood);
         return "redirect:/myRefg/list";  // 성공 시 커뮤니티 리스트 화면으로 redirect
         
     } catch (Exception e) {     // 예외 발생 시 입력 form으로 forwarding
