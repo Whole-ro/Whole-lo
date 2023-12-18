4a0209c75ccac846e84739f1f822d5e04a0a1275
yjin-jo <dpwls5100@icloud.com>
2023-12-18 15:22:33 +0900

    [feat] : userId param 추가 #25

--- whole/src/main/java/model/dao/mybatis/mapper/StatisticsMapper.java
+++ whole/src/main/java/model/dao/mybatis/mapper/StatisticsMapper.java
@@ -6,20 +6,20 @@ import org.apache.ibatis.annotations.Select;
 import model.dto.FoodDTO;
 public interface StatisticsMapper {
 	
-	@Select({"SELECT title FROM FOOD JOIN POST USING (post_id) WHERE is_healthy = 'RED'"})
+	@Select({"SELECT title FROM FOOD JOIN POST USING (post_id) WHERE is_healthy = 'RED' AND writer_id = #{userId}"})
 	List<FoodDTO> selectRedByUserId(Long userId);
 	
-	@Select({"SELECT title FROM FOOD JOIN POST USING (post_id) WHERE is_healthy = 'BLUE'"})
+	@Select({"SELECT title FROM FOOD JOIN POST USING (post_id) WHERE is_healthy = 'BLUE' AND writer_id = #{userId}"})
 	List<FoodDTO> selectBlueByUserId(Long userId);
 	
-	@Select({"SELECT COUNT(*) as cnt FROM FOOD JOIN POST USING (post_id) WHERE is_healthy = 'RED'"})
+	@Select({"SELECT COUNT(*) as cnt FROM FOOD JOIN POST USING (post_id) WHERE is_healthy = 'RED' AND writer_id = #{userId}"})
 	int countRedByUserId(Long userId);
 	
-	@Select({"SELECT COUNT(*) as cnt FROM FOOD JOIN POST USING (post_id) WHERE is_healthy = 'BLUE'"})
+	@Select({"SELECT COUNT(*) as cnt FROM FOOD JOIN POST USING (post_id) WHERE is_healthy = 'BLUE' AND writer_id = #{userId}"})
 	int countBlueByUserId(Long userId);
 	
 	@Select({"SELECT food_type\r\n"
 			+ "FROM (SELECT food_type FROM FOOD JOIN POST USING (post_id) GROUP BY food_type ORDER BY COUNT(food_type) DESC)\r\n"
-			+ "WHERE ROWNUM = 1"})
+			+ "WHERE ROWNUM = 1 AND writer_id = #{userId}"})
 	String findMostFoodType(Long userId);
 }
