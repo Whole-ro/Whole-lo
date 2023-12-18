d9290729f91518a4036bbe8e2b83a5bffaed9d75
dev_me <gjalsdud1030@naver.com>
2023-12-18 15:29:21 +0900

    [add] : statics Manager 추가 #25

---
+++ whole/src/main/java/model/service/FoodstaticsManager.java
@@ -0,0 +1,13 @@
+package model.service;
+
+import model.dao.mybatis.StatisMapperRepository;
+
+public class FoodstaticsManager {
+
+	private static FoodstaticsManager fstaticsMan = new FoodstaticsManager();
+	private StatisMapperRepository statismapperDAO;
+	
+	
+	
+	
+	}
