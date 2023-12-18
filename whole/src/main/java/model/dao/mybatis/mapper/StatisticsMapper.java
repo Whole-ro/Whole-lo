0be729ba3477c37f1a1be079c185ff29a4d46a49
yjin-jo <dpwls5100@icloud.com>
2023-12-18 13:00:10 +0900

    [add] : Most Food 쿼리 정의 #25

--- whole/src/main/java/model/dao/mybatis/mapper/StatisticsMapper.java
+++ whole/src/main/java/model/dao/mybatis/mapper/StatisticsMapper.java
@@ -18,4 +18,8 @@ public interface StatisticsMapper {
 	@Select({"SELECT COUNT(*) as cnt FROM FOOD JOIN POST USING (post_id) WHERE is_healthy = 'BLUE'"})
 	int countBlueByUserId(Long userId);
 	
+	@Select({"SELECT food_type\r\n"
+			+ "FROM (SELECT food_type FROM FOOD JOIN POST USING (post_id) GROUP BY food_type ORDER BY COUNT(food_type) DESC)\r\n"
+			+ "WHERE ROWNUM = 1"})
+	String findMostFoodType(Long userId);
 }
