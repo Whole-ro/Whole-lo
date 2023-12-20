4789b4f00a3742b1df2ae4e5632343484f115cf9
yjin-jo <dpwls5100@icloud.com>
2023-12-20 21:39:44 +0900

    [feat] : mapper에 userId param 추가 #46

--- whole/src/main/java/model/dao/mybatis/mapper/WithwithMapper.java
+++ whole/src/main/java/model/dao/mybatis/mapper/WithwithMapper.java
@@ -1,12 +1,15 @@
 package model.dao.mybatis.mapper;
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import model.dto.FoodDTO;
 import model.entity.UserEntity;
 public interface WithwithMapper {
 	
-	@Select({"SELECT nickname FROM USER_TABLE WHERE interest = #{interest}"})
-	List<UserEntity> selectUserByInterest(String interest);
+	@Select({"SELECT nickname, address, introduction FROM USER_TABLE WHERE interest = #{interest} AND user_id != #{userId}"})
+	List<UserEntity> selectUserByInterest(
+			@Param("interest")String interest, 
+			@Param("userId") long userId);
 }
