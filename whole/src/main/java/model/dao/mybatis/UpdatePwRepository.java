5921a351f2aad2e55d78558d798156a44ae2aaef
dev_me <gjalsdud1030@naver.com>
2023-12-22 00:18:20 +0900

    [feat] : 비밀번호 변경 기능 구현 완료 #58

--- whole/src/main/java/model/dao/mybatis/UpdatePwRepository.java
+++ whole/src/main/java/model/dao/mybatis/UpdatePwRepository.java
@@ -35,6 +35,7 @@ public class UpdatePwRepository {
 		        sqlSession.commit();
 		        // Log the result
 		        System.out.println("result: "+result);
+		     
 
 		        return result;
 		} finally {
