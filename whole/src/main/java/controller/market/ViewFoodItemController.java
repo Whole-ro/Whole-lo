ae30bce2336e5000db5ed26bd85c956eea7d98dc
yjin-jo <dpwls5100@icloud.com>
2023-12-21 15:02:27 +0900

    [add] : ViewFoodItemController 추가 #55

---
+++ whole/src/main/java/controller/market/ViewFoodItemController.java
@@ -0,0 +1,25 @@
+package controller.market;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import controller.Controller;
+import model.entity.ItemEntity;
+import model.service.MarketManager;
+
+public class ViewFoodItemController implements Controller{
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		MarketManager marketMan = MarketManager.getInstance();
+		List<ItemEntity> marketList = marketMan.viewFoodItemList();
+		
+		// commList 객체를 request에 저장하여 커뮤니티 리스트 화면으로 이동(forwarding)
+		request.setAttribute("marketList", marketList);				
+		return "/market/market_view_postlist.jsp";        
+	}
+
+}
