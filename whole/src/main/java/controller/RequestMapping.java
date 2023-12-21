6dde6d682f3e51d8078f8ce2405f5b087628ad49
yjin-jo <dpwls5100@icloud.com>
2023-12-21 15:04:05 +0900

    [feat] : /market/food 요청 매핑 #55

--- whole/src/main/java/controller/RequestMapping.java
+++ whole/src/main/java/controller/RequestMapping.java
@@ -19,66 +19,66 @@ import controller.food.ViewFoodController;
 import controller.market.*;
 
 public class RequestMapping {
-    private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
-    
-    // 각 요청 uri에 대한 controller 객체를 저장할 HashMap 생성
-    private Map<String, Controller> mappings = new HashMap<String, Controller>();
+	private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
 
-    public void initMapping() {
-    	// 각 uri에 대응되는 controller 객체를 생성 및 저장
-//        mappings.put("/", new ForwardController("index.jsp"));
-         mappings.put("/user/login/form", new ForwardController("/user/loginForm.jsp"));
-         mappings.put("/user/login", new LoginController());
-        
-//        mappings.put("/user/login/form", new ForwardController("/user/loginForm.jsp"));
-//        mappings.put("/user/login", new LoginController());
-//        mappings.put("/user/logout", new LogoutController());
-         mappings.put("/market", new ListMarketController());
-         mappings.put("/market/post/detail", new ViewPostController());
-//        mappings.put("/user/list", new ListUserController());
-        mappings.put("/user/view", new ViewUserController());
-//        
-        
-        // 회원 가입 폼 요청과 가입 요청 처리 병합 (폼에 커뮤니티 선택 메뉴 추가를 위함)
-        mappings.put("/user/register/agreeTerms", new ForwardController("/user/sign_up_1.jsp"));
-        mappings.put("/user/register/registerForm", new ForwardController("/user/sign_up_2.jsp"));
-        mappings.put("/user/register", new RegisterUserController());
+	// 각 요청 uri에 대한 controller 객체를 저장할 HashMap 생성
+	private Map<String, Controller> mappings = new HashMap<String, Controller>();
 
-//        // 사용자 정보 수정 폼 요청과 수정 요청 처리 병합
-////      mappings.put("/user/update/form", new UpdateUserFormController());
-////      mappings.put("/user/update", new UpdateUserController());        
-//        mappings.put("/user/update", new UpdateUserController());
-//        
-//        mappings.put("/user/delete", new DeleteUserController());
-//        
-//        // 커뮤니티 관련 request URI 추가
-//        mappings.put("/community/list", new ListCommunityController());
-//        mappings.put("/community/view", new ViewCommunityController());
-//        mappings.put("/community/create/form", new ForwardController("/community/creationForm.jsp"));
-//        mappings.put("/community/create", new CreateCommunityController());
-//        mappings.put("/community/update", new UpdateCommunityController());
-        
-    
-        // 나만의 냉장rh
-        mappings.put("/myRefg/list", new ListFoodController());
-        mappings.put("/myRefg/postForm", new ForwardController("/myRefg/postForm.jsp"));
-        mappings.put("/myRefg/list/detail", new ViewFoodController());
-        mappings.put("/myRefg/post", new FoodCreateController());
-        mappings.put("/myRefg/delete", new FoodDeleteController());
+	public void initMapping() {
+		// 각 uri에 대응되는 controller 객체를 생성 및 저장
+		//        mappings.put("/", new ForwardController("index.jsp"));
+		mappings.put("/user/login/form", new ForwardController("/user/loginForm.jsp"));
+		mappings.put("/user/login", new LoginController());
 
-mappings.put("/myRefg/search/title", new SearchFoodTitleController());
-mappings.put("/myRefg/post/update", new FoodUpdateController());
-mappings.put("/myRefg/search/exp", new SearchFoodExpController());
+		//        mappings.put("/user/login/form", new ForwardController("/user/loginForm.jsp"));
+		//        mappings.put("/user/login", new LoginController());
+		//        mappings.put("/user/logout", new LogoutController());
+		mappings.put("/market", new ListMarketController());
+		mappings.put("/market/post/detail", new ViewPostController());
+		//        mappings.put("/user/list", new ListUserController());
+		mappings.put("/user/view", new ViewUserController());
+		mappings.put("/market/food", new ViewFoodItemController());		//        
+
+		// 회원 가입 폼 요청과 가입 요청 처리 병합 (폼에 커뮤니티 선택 메뉴 추가를 위함)
+		mappings.put("/user/register/agreeTerms", new ForwardController("/user/sign_up_1.jsp"));
+		mappings.put("/user/register/registerForm", new ForwardController("/user/sign_up_2.jsp"));
+		mappings.put("/user/register", new RegisterUserController());
+
+		//        // 사용자 정보 수정 폼 요청과 수정 요청 처리 병합
+		////      mappings.put("/user/update/form", new UpdateUserFormController());
+		////      mappings.put("/user/update", new UpdateUserController());        
+		//        mappings.put("/user/update", new UpdateUserController());
+		//        
+		//        mappings.put("/user/delete", new DeleteUserController());
+		//        
+		//        // 커뮤니티 관련 request URI 추가
+		//        mappings.put("/community/list", new ListCommunityController());
+		//        mappings.put("/community/view", new ViewCommunityController());
+		//        mappings.put("/community/create/form", new ForwardController("/community/creationForm.jsp"));
+		//        mappings.put("/community/create", new CreateCommunityController());
+		//        mappings.put("/community/update", new UpdateCommunityController());
 
 
-        //같이가치
-        mappings.put("/withWith", new ListSameInterestUserController());
-        mappings.put("/withWith/gender", new ListSameGenderController());
-        logger.info("Initialized Request Mapping!");
-    }
+		// 나만의 냉장rh
+		mappings.put("/myRefg/list", new ListFoodController());
+		mappings.put("/myRefg/postForm", new ForwardController("/myRefg/postForm.jsp"));
+		mappings.put("/myRefg/list/detail", new ViewFoodController());
+		mappings.put("/myRefg/post", new FoodCreateController());
+		mappings.put("/myRefg/delete", new FoodDeleteController());
 
-    public Controller findController(String uri) {	
-    	// 주어진 uri에 대응되는 controller 객체를 찾아 반환
-        return mappings.get(uri);
-    }
-    }
+		mappings.put("/myRefg/search/title", new SearchFoodTitleController());
+		mappings.put("/myRefg/post/update", new FoodUpdateController());
+		mappings.put("/myRefg/search/exp", new SearchFoodExpController());
+
+
+		//같이가치
+		mappings.put("/withWith", new ListSameInterestUserController());
+		mappings.put("/withWith/gender", new ListSameGenderController());
+		logger.info("Initialized Request Mapping!");
+	}
+
+	public Controller findController(String uri) {	
+		// 주어진 uri에 대응되는 controller 객체를 찾아 반환
+		return mappings.get(uri);
+	}
+}
