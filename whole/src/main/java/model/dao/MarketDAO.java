0092b32ea5d476fac56ca344e2d3a047021694fd
yjin-jo <dpwls5100@icloud.com>
2023-12-21 19:07:26 +0900

    [feat] : item_type 추가 #55

--- whole/src/main/java/model/dao/MarketDAO.java
+++ whole/src/main/java/model/dao/MarketDAO.java
@@ -182,7 +182,7 @@ public class MarketDAO {
     * 저장하여 반환.
     */
    public ItemEntity findItem(Long postId) throws SQLException {
-      String sql = "SELECT title, price, content, image, reg_date "
+      String sql = "SELECT title, price, content, image, reg_date, item_type "
             + "FROM ITEM JOIN POST USING (post_id) "
             + "WHERE post_id=? "
             + "ORDER BY reg_date"; 
@@ -197,6 +197,7 @@ public class MarketDAO {
             item.setPrice(rs.getLong("price"));
             item.setContent(rs.getString("content"));
             item.setImage(rs.getString("image"));
+            item.setItemType(rs.getString("item_type"));
             item.setRegDate(rs.getDate("reg_Date").toLocalDate());
          }
       } catch (Exception ex) {
